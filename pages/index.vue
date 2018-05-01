8b93861eb8a28fa9fca27137ec9b1a5cf6395d3d
Eric Hodges <ehodges2004@gmail.com>
2018-05-01 13:22:28 -0700

    added basic navbar

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,56 @@
 <template>
-  <section class="container">
-    <div>      
-      <h1 class="title">
-        Better Sleep Through Yoga
-      </h1>
-      <h2 class="subtitle">
-        Practice a custom yoga program to help you sleep
-      </h2>
-      <div class="links">
-        <a
-          href="#"
-          target="_blank"
-          class="button--green">Get Started</a>
+  <main>
+    <section class="section-1">
+      <ul class="ul-nav">
+        <li class="li-logo">
+          <img src="~/assets/images/logo.png" alt="Trillo Logo" class="logo">
+        </li>
         
-      </div>
-    </div>
-  </section>
+        <li class="li-user-nav">Products</li>
+        <li class="li-user-nav">About</li>
+        <li class="li-user-nav">Contact</li>
+
+      </ul>
+      
+      <!-- <div class="nav-div">
+        <div class="header">
+          <div class="logo-box">
+            <img src="~/assets/images/logo.png" alt="Trillo Logo" class="logo">
+          </div>          
+          <div class="user-nav">
+            <div class="user-nav-text">
+              products
+            </div>
+            <div class="user-nav-text">
+              about
+            </div>
+            <div class="user-nav-text">
+              contact
+            </div>
+          </div>
+        </div>
+      </div> -->
+
+      <!-- <div>      
+        <h1 class="title">
+          Better Sleep Through Yoga
+        </h1>
+        <h2 class="subtitle">
+          A customized yoga program to help you sleep
+        </h2>
+        <div class="links">
+          <a
+            href="#"
+            target="_blank"
+            class="button--green">Get Started</a>
+          
+        </div>
+      </div> -->
+    </section>
+    <section>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur pariatur molestiae magni veniam dignissimos, provident officia fuga quia culpa amet ullam quasi impedit! Tenetur voluptatum commodi tempora molestias ratione facilis id fugiat, cupiditate velit magnam nesciunt. Sint iste repellat exercitationem! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est rerum facere perferendis quisquam porro aspernatur veniam minus dolor! Dolorum repudiandae error dicta corrupti velit similique alias? Quibusdam ipsa, quis tenetur, perferendis dolorum vitae reprehenderit quae doloremque debitis rem excepturi dolor placeat numquam modi dignissimos neque beatae libero qui iste aut explicabo fugit similique distinctio quod. Perferendis maxime repellat obcaecati omnis, et nobis impedit quos modi, cum, tenetur soluta eveniet sed delectus natus dolore accusamus temporibus. Illo odio omnis modi ad reprehenderit, aperiam, ab molestias consectetur at nulla perspiciatis dicta aliquam nemo quod earum laborum asperiores possimus distinctio quam labore odit culpa optio repellendus? Omnis officia pariatur, quibusdam consectetur cum magnam! Odio, culpa. Doloribus aliquam reiciendis nostrum doloremque rem temporibus consequatur natus, sapiente reprehenderit nihil. Assumenda tempora atque aperiam minima dolorem dignissimos necessitatibus odio quibusdam! Unde, nisi. Obcaecati veritatis cupiditate, facere dolorem tempora, qui necessitatibus ad possimus voluptas eos ullam velit vero ipsam, quam pariatur ipsum quasi error soluta dolore praesentium! Exercitationem, placeat neque quo illum nemo cum ducimus est dicta natus illo, voluptatem doloribus rem ipsum numquam alias explicabo. Aliquid id culpa incidunt nihil sint quia enim reprehenderit ipsum odio adipisci. Dicta quam voluptate eos excepturi ipsam molestiae provident officia.
+    </section>
+  </main>
 </template>
 
-<script>
 
-</script>
-
-<style>
-
-</style>
