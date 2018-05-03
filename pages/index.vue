cd8a360d39095c4933a29687011b9932aa781f9a
Eric Hodges <ehodges2004@gmail.com>
2018-05-03 15:34:25 -0700

    page down issue with 100vh

--- pages/index.vue
+++ pages/index.vue
@@ -14,10 +14,10 @@
 
       <div class="section-1__heading">      
         <h1 class="title">
-          Better Sleep Through Yoga
+          Trouble sleeping?
         </h1>
         <h2 class="subtitle">
-          A customized yoga program to help you sleep
+          Try a customized sleep therapy
         </h2>
         <div class="links">
           <a
@@ -28,8 +28,8 @@
       </div>
     </section>
 
-    <section>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur pariatur molestiae magni veniam dignissimos, provident officia fuga quia culpa amet ullam quasi impedit! Tenetur voluptatum commodi tempora molestias ratione facilis id fugiat, cupiditate velit magnam nesciunt. Sint iste repellat exercitationem! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est rerum facere perferendis quisquam porro aspernatur veniam minus dolor! Dolorum repudiandae error dicta corrupti velit similique alias? Quibusdam ipsa, quis tenetur, perferendis dolorum vitae reprehenderit quae doloremque debitis rem excepturi dolor placeat numquam modi dignissimos neque beatae libero qui iste aut explicabo fugit similique distinctio quod. Perferendis maxime repellat obcaecati omnis, et nobis impedit quos modi, cum, tenetur soluta eveniet sed delectus natus dolore accusamus temporibus. Illo odio omnis modi ad reprehenderit, aperiam, ab molestias consectetur at nulla perspiciatis dicta aliquam nemo quod earum laborum asperiores possimus distinctio quam labore odit culpa optio repellendus? Omnis officia pariatur, quibusdam consectetur cum magnam! Odio, culpa. Doloribus aliquam reiciendis nostrum doloremque rem temporibus consequatur natus, sapiente reprehenderit nihil. Assumenda tempora atque aperiam minima dolorem dignissimos necessitatibus odio quibusdam! Unde, nisi. Obcaecati veritatis cupiditate, facere dolorem tempora, qui necessitatibus ad possimus voluptas eos ullam velit vero ipsam, quam pariatur ipsum quasi error soluta dolore praesentium! Exercitationem, placeat neque quo illum nemo cum ducimus est dicta natus illo, voluptatem doloribus rem ipsum numquam alias explicabo. Aliquid id culpa incidunt nihil sint quia enim reprehenderit ipsum odio adipisci. Dicta quam voluptate eos excepturi ipsam molestiae provident officia.
+    <section class="section-2">
+      
     </section>
   </main>
 </template>
