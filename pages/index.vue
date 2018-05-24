7705c8de977b5b2a98dcda9dc02ccac125e9b303
Eric Hodges <ehodges2004@gmail.com>
2018-05-24 15:18:49 -0700

    added bounce animation for down arrow

--- pages/index.vue
+++ pages/index.vue
@@ -18,9 +18,9 @@
           </a>          
         </div>
       <!-- </div> -->
-      <div class="down-arrow-div  ">
+      <div class="down-arrow-div ">
         <a href="#" v-scroll-to="'#section-2'">
-          <svg class="down-arrow" fill="#FFFFFF" height="60" viewBox="0 0 24 24" width="60" xmlns="http://www.w3.org/2000/svg">
+          <svg class="down-arrow bounce" fill="#FFFFFF" height="60" viewBox="0 0 24 24" width="60" xmlns="http://www.w3.org/2000/svg">
           <path d="M7.41 7.84L12 12.42l4.59-4.58L18 9.25l-6 6-6-6z"/>
           <path d="M0-.75h24v24H0z" fill="none"/>
         </svg>
